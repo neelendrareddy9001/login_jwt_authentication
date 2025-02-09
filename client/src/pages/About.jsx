8fc02ef72e5768ea506739d250ed63f594b03cfb
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2025-02-09 23:58:11 +0100

    route to main page install react-router-dom

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div className="section-container">
       <h2>About</h2>
+      <Link to="/">
+        <button className="back_main">Back to Main Page</button>
+      </Link>
     </div>
   );
 };
