63852a9e828884235c0323c92d89a0f5537af892
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2025-03-06 23:25:33 +0100

    added

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="section-container">
-      <h1>Contact</h1>
+      <h1>Contact Us</h1>
+      <form>
+        <div className="input-container">
+          <label htmlFor="name">Name</label>
+          <input type="text" placeholder="Enter your name" />
+        </div>
+      </form>
     </div>
   );
 };
