64e72a7cae82d157d4654577a68be090f51e3970
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2025-03-08 23:49:51 +0100

    custom media query tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      smm: "540px",
+      mdd: "800px",
+      lgg: "992px",
+      xll: "1040px",
+    },
   },
   plugins: [],
 };
